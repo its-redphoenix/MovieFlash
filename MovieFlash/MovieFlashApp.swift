874cf1b1122dc034209911758bea5d939cbe2774
Mayur <mayur@redphoenix.co.in>
2020-08-04 21:05:24 +0530

    Initial Commit

---
+++ MovieFlash/MovieFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlashApp.swift
+//  MovieFlash
+//
+//  Created by Mayur on 04/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
